7f6194b46a027d5d69543b2618a770f8eca7c147
nikhred <allolanikhil@gmail.com>
2026-01-22 20:19:18 +0530

    updated bypass modes with new adc + dac mapping

--- pl/srcs/rtl/axis_demux.v
+++ pl/srcs/rtl/axis_demux.v
@@ -37,9 +37,24 @@ module axis_demux #(
     input  wire            m2_tready
 );
 
-    wire [DW - 1:0] s_tdata_i;
-    wire            s_tvalid_i;
-    reg             s_tready_i;
+    wire       [DW - 1:0] s_tdata_i;
+    wire                  s_tvalid_i;
+    reg                   s_tready_i;
+
+    // signals for rx bypass
+    reg signed [    15:0] in_i       [0:6] [0:1];
+    reg signed [    15:0] in_q       [0:6] [0:1];
+
+    reg signed [    15:0] out_i      [0:1];
+    reg signed [    15:0] out_q      [0:1];
+
+    reg signed [    16:0] sum0_i     [0:3] [0:1];
+    reg signed [    17:0] sum1_i     [0:1] [0:1];
+    reg signed [    18:0] sum_i      [0:1];
+
+    reg signed [    16:0] sum0_q     [0:3] [0:1];
+    reg signed [    17:0] sum1_q     [0:1] [0:1];
+    reg signed [    18:0] sum_q      [0:1];
 
     axis_skidbuffer #(
         .DW(DW)
@@ -77,29 +92,80 @@ module axis_demux #(
                 m1_tdata   = s_tdata_i;
                 m1_tvalid  = s_tvalid_i;
             end
-            2: begin  // bypass mode
+            2: begin  // bypass mode - tx + rx 
                 s_tready_i = m2_tready;
-                // m2_tdata   = s_tdata_i;
-                // route adc0 data to all dacs
-                for (i = 0; i < 8; i = i + 1) begin
-                    m2_tdata[64*i+0+:16]  = s_tdata_i[0+:16];  // i0  -> i0
-                    m2_tdata[64*i+16+:16] = s_tdata_i[16+:16];  // i1  -> q0
-                    m2_tdata[64*i+32+:16] = s_tdata_i[32+:16];  // q0  -> i1
-                    m2_tdata[64*i+48+:16] = s_tdata_i[48+:16];  // q1  -> q1
+                // tx bypass: adc0 to dac1 to dac7
+                // mapping from ADC: q1 q0 i1 i0
+                // mapping to DAC: i1 q1 i0 q0                
+                for (i = 1; i < 8; i = i + 1) begin
+                    m2_tdata[64*i+16+:16]  = s_tdata_i[0+:16];  // i0  -> i0
+                    m2_tdata[64*i+48+:16] = s_tdata_i[16+:16];  // i1  -> q0
+                    m2_tdata[64*i+0+:16] = s_tdata_i[32+:16];  // q0  -> i1
+                    m2_tdata[64*i+32+:16] = s_tdata_i[48+:16];  // q1  -> q1
                 end
+                // rx bypass: sum(adc1:adc7) -> dac0
+                m2_tdata[0 +: 64]   = {
+                    out_i[1],                     
+                    out_q[1], 
+                    out_i[0],
+                    out_q[0]
+                };
                 m2_tvalid = s_tvalid_i;
             end
-            3: begin  // bypass mode - different mapping
+            3: begin  // bypass mode - tx bypass
                 s_tready_i = m2_tready;
                 m2_tvalid  = s_tvalid_i;
+                // mapping from ADC: q1 q0 i1 i0
+                // mapping to DAC: i1 q1 i0 q0
                 for (i = 0; i < 8; i = i + 1) begin
-                    m2_tdata[64*i+0+:16]  = s_tdata_i[0+:16];  // i0  -> i0
-                    m2_tdata[64*i+16+:16] = s_tdata_i[32+:16];  // i1  -> q0
-                    m2_tdata[64*i+32+:16] = s_tdata_i[16+:16];  // q0  -> i1
-                    m2_tdata[64*i+48+:16] = s_tdata_i[48+:16];  // q1  -> q1
+                    m2_tdata[64*i+16+:16]  = s_tdata_i[0+:16];  // i0  -> i0
+                    m2_tdata[64*i+48+:16] = s_tdata_i[16+:16];  // i1  -> q0
+                    m2_tdata[64*i+0+:16] = s_tdata_i[32+:16];  // q0  -> i1
+                    m2_tdata[64*i+32+:16] = s_tdata_i[48+:16];  // q1  -> q1
                 end
             end
         endcase
     end
 
+    // pipelined in clog2() stages
+    always @(posedge clk) begin
+        for (i = 0; i < 2; i=i+1) begin
+            // real part summation
+            sum0_i[0][i] <= in_i[0][i] + in_i[1][i];
+            sum0_i[1][i] <= in_i[2][i] + in_i[3][i];
+            sum0_i[2][i] <= in_i[4][i] + in_i[5][i];
+            sum0_i[3][i] <= in_i[6][i];  // ignore linter warning
+
+            sum1_i[0][i] <= sum0_i[0][i] + sum0_i[1][i];
+            sum1_i[1][i] <= sum0_i[2][i] + sum0_i[3][i];
+
+            sum_i[i]     <= sum1_i[0][i] + sum1_i[1][i];
+
+            // imag part summation
+            sum0_q[0][i] <= in_q[0][i] + in_q[1][i];
+            sum0_q[1][i] <= in_q[2][i] + in_q[3][i];
+            sum0_q[2][i] <= in_q[4][i] + in_q[5][i];
+            sum0_q[3][i] <= in_q[6][i];  // ignore linter warning
+
+            sum1_q[0][i] <= sum0_q[0][i] + sum0_q[1][i];
+            sum1_q[1][i] <= sum0_q[2][i] + sum0_q[3][i];
+
+            sum_q[i]     <= sum1_q[0][i] + sum1_q[1][i];
+        end
+    end
+
+    always @(*) begin
+        for (i = 0; i < 7; i=i+1) begin
+            // mapping starts from adc1 (64 + )
+            in_i[i][0] = s_tdata_i[64+64*i+:16];
+            in_q[i][0] = s_tdata_i[64+64*i+32+:16];
+
+            in_i[i][1] = s_tdata_i[64+64*i+16+:16];
+            in_q[i][1] = s_tdata_i[64+64*i+48+:16];
+        end
+        for (i = 0; i < 2; i=i+1) begin
+            out_i[i] = sum_i[i];  // >>> 3;
+            out_q[i] = sum_q[i];  // >>> 3;
+        end
+    end
 endmodule
